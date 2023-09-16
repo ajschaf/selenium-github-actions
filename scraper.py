ef35d8b8864922ebf30d9e0dd1ac4555aa9750bc
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2023-09-16 17:39:40 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
@@ -18,6 +18,8 @@ options = [
 ]
 for option in options:
     chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 driver.get('http://nytimes.com')
 print(driver.title)
